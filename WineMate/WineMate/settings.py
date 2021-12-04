493a3ab12f812fe4b0f08e81ce5d1df5aadce753
VladimirValkov <57315074+VladimirValkov@users.noreply.github.com>
2021-12-04 13:18:46 +0200

    fix

--- WineMate/WineMate/settings.py
+++ WineMate/WineMate/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = "/home/teamppmg/project/WineMate/table/static"
+
 STATIC_URL = '/static/'
 
 
