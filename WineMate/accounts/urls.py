8cecfe5eebd38444d2e8292b30f93549bf3e5f11
VladimirValkov <57315074+VladimirValkov@users.noreply.github.com>
2022-03-17 23:17:31 +0200

    fix

--- WineMate/accounts/urls.py
+++ WineMate/accounts/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Login.as_view(), name='login'),
-    path('register', views.Register.as_view(), name='register'),
     path('login', views.Login.as_view(), name='login')
 ]
