6a77b96755a53e3a9090bd1a3bf2362c5f9d6598
VladimirValkov <57315074+VladimirValkov@users.noreply.github.com>
2022-03-17 23:37:04 +0200

    fix

--- WineMate/accounts/views.py
+++ WineMate/accounts/views.py
@@ -2,19 +2,19 @@ from django.contrib.auth import views as auth_views
 from django.shortcuts import resolve_url
 
 
-class Register(View):
-    def get(self, request):
-        register_form = UserCreationForm()
-        context = {'form': register_form}
-        return render(request, 'accounts/register.html', context)
+# class Register(View):
+#     def get(self, request):
+#         register_form = UserCreationForm()
+#         context = {'form': register_form}
+#         return render(request, 'accounts/register.html', context)
 
-    def post(self, request):
-        register_form = UserCreationForm(request.POST)
-        if register_form.is_valid():
-            register_form.save()
+#     def post(self, request):
+#         register_form = UserCreationForm(request.POST)
+#         if register_form.is_valid():
+#             register_form.save()
 
-        context = {'form': register_form}
-        return render(request, 'accounts/register.html', context)
+#         context = {'form': register_form}
+#         return render(request, 'accounts/register.html', context)
 
 class Login(auth_views.LoginView):
     template_name = 'accounts/login.html'
